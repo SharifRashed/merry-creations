89d45db0fb4dcf370ce97d69f4dd929ec8a76c6f
SharifRashed <sharif.rashed26@gmail.com>
2021-09-20 11:29:02 -0500

    json and index edits to display site

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { MerryCreations } from './components/MerryCreations';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <MerryCreations />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

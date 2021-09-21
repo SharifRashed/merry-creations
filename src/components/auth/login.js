d17e7379f458bf2750d8fabfe0e3945457c292f5
SharifRashed <sharif.rashed26@gmail.com>
2021-09-21 08:46:27 -0500

    fixed login issues and added myself as a user

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -19,7 +19,7 @@ export const Login = () => {
         existingUserCheck()
             .then(exists => {
                 if (exists) {
-                    localStorage.setItem("honey_customer", exists.id)
+                    localStorage.setItem("merry_user", exists.id)
                     history.push("/")
                 } else {
                     existDialog.current.showModal()
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 
-export const users = () => {
+export const Users = () => {
     const [users, modifyUsers] = useState([])
     const [totalUserMessage, updateMessage] = useState("")
 
